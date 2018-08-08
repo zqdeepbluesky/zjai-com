32d5add50093e31f23ad00211f5b5a60d558c9ae
fmsunyh <fmsunyh@gmail.com>
2018-08-08 15:27:30 +0800

    00019 - 添加用户训练setting.cfg, 覆盖默认值，避免多用户配置问题 type error.

--- lib/roi_data_layer/minibatch.py
+++ lib/roi_data_layer/minibatch.py
@@ -24,8 +24,8 @@ def get_minibatch(roidb, num_classes):
     random_scale_inds = npr.randint(0, high=len(cfg.TRAIN.SCALES), size=num_images)  #随机选择区间内的整数
     if cfg.TRAIN.BATCH_SIZE % num_images != 0:
         print('num_images ({}) must divide BATCH_SIZE ({})'.format(num_images, cfg.TRAIN.BATCH_SIZE))
-		assert(cfg.TRAIN.BATCH_SIZE % num_images == 0)
-    # assert(cfg.TRAIN.BATCH_SIZE % num_images == 0),'num_images ({}) must divide BATCH_SIZE ({})'.format(num_images, cfg.TRAIN.BATCH_SIZE)    #batchSize必须能够整除每次输入参数
+        assert(cfg.TRAIN.BATCH_SIZE % num_images == 0)
+        # assert(cfg.TRAIN.BATCH_SIZE % num_images == 0),'num_images ({}) must divide BATCH_SIZE ({})'.format(num_images, cfg.TRAIN.BATCH_SIZE)    #batchSize必须能够整除每次输入参数
 
     # Get the input image blob, formatted for caffe
     im_blob, im_scales = _get_image_blob(roidb, random_scale_inds)   #缩放后的图像，并装在blob，缩放系数
