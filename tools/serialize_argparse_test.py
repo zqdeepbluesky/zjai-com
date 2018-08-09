7c5a064384335caf5835f08791c0eb2a6d566889
fmsunyh <fmsunyh@gmail.com>
2018-08-09 17:00:29 +0800

    00027 - 序列化args，保存到本地

---
+++ tools/serialize_argparse_test.py
@@ -0,0 +1,26 @@
+# -----------------------------------------------------
+# -*- coding: utf-8 -*-
+# @Time    : 8/9/2018 4:34 PM
+# @Author  : sunyonghai
+# @Software: ZJ_AI
+# -----------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tools.serialize_argparse import SerializeArgparse
+import os
+from model.config import cfg
+
+parser = SerializeArgparse(description='FRCNN trainer.')
+parser.add_argument('-name', type=str, default=os.path.join(cfg.ROOT_DIR, "data/args_parse") , help="Directory to save args")
+parser.add_argument('-learningRate', type=float, default=0.0001, help='Learning rate')
+parser.add_argument('-gpu', type=str, default="0", help='Train on this GPU(s)')
+
+opt=parser.parse_args()
+
+if not os.path.isdir(opt.name):
+	os.makedirs(opt.name)
+
+opt = parser.load(opt.name+"/args.json")
+parser.save(opt.name+"/args.json")
