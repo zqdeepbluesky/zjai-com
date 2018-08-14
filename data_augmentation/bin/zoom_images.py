0ea31b117a78bfde08836064cbf83d092773893f
jaykky <15521306897@139.com>
2018-08-14 16:16:36 +0800

    00011 - 数据增强
    此部分为膨胀缩小部分的代码

--- data_augmentation/bin/zoom_images.py
+++ data_augmentation/bin/zoom_images.py
@@ -26,50 +26,41 @@ def zoom_images_and_save(scales,data_dir):
                 xml_path = os.path.join(annot_path, a + ".xml")
                 object_infos = xml_store.get_object_infos_from_xml(xml_path)
                 show_object_cv_box(object_infos, img)
-                zoom_img = scaleit(img,scale)
-                # object_infos_rotate=rotate_xml(object_infos,img,angle,rotated_img)
-                show_object_cv_box(object_infos, zoom_img)
+                zoom_img = zoom_image(img,scale[0],scale[1])
+                new_object_infos=zoom_xml(object_infos,img,scale)
+                show_object_cv_box(new_object_infos, zoom_img)
                 break
 
-                new_img_name=a + "_rotate_" + str(angle) + ".jpg"
-                new_img_path=os.path.join( data_dir+'_zoom_{}'.format(angle),'JPEGImages', new_img_name)
+                new_img_name=a + "_zoom_{}_{}".format(scale[0],scale[1]) + ".jpg"
+                new_img_path=os.path.join( data_dir+'_zoom_{}_{}'.format(scale[0],scale[1]),'JPEGImages', new_img_name)
 
 
                 # print(object_infos)
                 # show_object_cv_box(object_infos, rotated_img)
                 # print(rotated_img.shape)
-                im_info=xml_utils.create_image_info(new_img_name,new_img_path,rotated_img.shape[1],rotated_img.shape[0],rotated_img.shape[2])
-                new_xml_path=os.path.join( data_dir+'_zoom_{}'.format(angle),'Annotations')
+                im_info=xml_utils.create_image_info(new_img_name,new_img_path,zoom_img.shape[1],zoom_img.shape[0],zoom_img.shape[2])
+                new_xml_path=os.path.join( data_dir+'_zoom_{}_{}'.format(scale[0],scale[1]),'Annotations')
 
 
                 cv2.imwrite(new_img_path, rotated_img)
                 xml_store.save_annotations(new_xml_path, im_info, object_infos_rotate)
 
-def scaleit(image, factor_x,factor_y=1, isseg=False):
+def zoom_image(image, factor_x,factor_y, isseg=False):
     order = 0 if isseg == True else 3
-    newimg = interpolation.zoom(image, (float(factor_x), float(factor_y), 1.0), order=order, mode='nearest')
+    newimg = interpolation.zoom(image, (float(factor_y), float(factor_x), 1.0), order=order, mode='nearest')
     return newimg
 
-def rotate_xml(object_infos,img,angle,img_rotate):
-    rotate_obj_infos=[]
+def zoom_xml(object_infos,img,scale):
+    new_obj_infos=[]
     for object_info in object_infos:
         class_name,_,xmin, ymin, xmax, ymax = object_info.split(",")
-        x_list=[int(xmin),int(xmax)]
-        y_list=[int(ymin),int(ymax)]
-        height, width = img.shape[0], img.shape[1]
-        max_x,max_y=0,0
-        min_x,min_y=width,height
-        for x in x_list:
-            for y in y_list:
-                x1, y1 = rotate_point(width,height, angle, x, y)
-                x1 =int(x1+(img_rotate.shape[1]/2))
-                y1 =int(y1+(img_rotate.shape[0]/2))
-                max_x = max([max_x, x1])
-                min_x = min([min_x, x1])
-                max_y = max([max_y, y1])
-                min_y = min([min_y, y1])
-        rotate_obj_infos.append("{},1,{},{},{},{}".format(class_name,min_x,min_y,max_x,max_y))
-    return rotate_obj_infos
+        min_x = int(int(xmin)*scale[0])
+        min_y = int(int(ymin) * scale[1])
+        max_x = int(int(xmax) * scale[0])
+        max_y = int(int(ymax) * scale[1])
+
+        new_obj_infos.append("{},1,{},{},{},{}".format(class_name,min_x,min_y,max_x,max_y))
+    return new_obj_infos
 
 
 def parse_args():
@@ -80,7 +71,7 @@ def parse_args():
     parser.add_argument('--package_dir', dest='package_dir', help='the compare data file name',
                         default="train_data-2018-03-07")
     parser.add_argument('--zoom_scale', dest='zoom_scale', help='how many the num you want to split',
-                        default=[2])
+                        default=[(1,2)])
     args = parser.parse_args()
 
     return args
